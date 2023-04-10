d68cf9ad9be545d5e48ed3f28d92c22171172a5e
Roberto Esteves <robertoesteves@protonmail.com>
2023-04-10 12:09:15 -0300

    transforming: combining various transforms

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "glm/ext/matrix_transform.hpp"
 #include <cmath>
 #include <cstring>
 #include <stdio.h>
@@ -10,8 +11,21 @@
 #include <glm/gtc/type_ptr.hpp>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = M_PI / 180.0f;
 
 GLuint VAO, VBO, shader, uniformModel;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.005f;
+
+float curAngle = 0.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 // Vertex Shader
 static const char* vShader =
@@ -22,7 +36,7 @@ static const char* vShader =
     "uniform mat4 model;\n"
     "\n"
     "void main() {\n"
-    "   gl_Position = model * vec4(pos, 1.0);\n"
+    "   gl_Position = model * vec4(pos.x, pos.y, pos.z, 1.0);\n"
     "}\0";
 
 static const char* fShader =
@@ -160,13 +174,36 @@ int main() {
     while(!glfwWindowShouldClose(mainWindow)) {
         glfwPollEvents();
 
+        if (direction)
+            triOffset += triIncrement;
+        else
+            triOffset -= triIncrement;
+
+        if (std::abs(triOffset) >= triMaxOffset)
+            direction = !direction;
+
+        curAngle += 0.05f;
+        if (curAngle >= 360) {
+            curAngle -= 360;
+        }
+
+        if (sizeDirection)
+            curSize += 0.001f;
+        else
+            curSize -= 0.001f;
+
+        if (curSize >= maxSize || curSize <= minSize)
+            sizeDirection = !sizeDirection;
+
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
 
         glm::mat4 model(1.0f);
-        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+        model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
 
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
