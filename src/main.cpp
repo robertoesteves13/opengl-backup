0610ee1db567fe854cdbdc5d24f8a8b535247cd3
Roberto Esteves <robertoesteves@protonmail.com>
2023-04-10 19:26:59 -0300

    transform: colour interpolation

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,8 @@
 #include <glm/gtc/type_ptr.hpp>
 
 const GLint WIDTH = 800, HEIGHT = 600;
-const float toRadians = M_PI / 180.0f;
 
 GLuint VAO, VBO, shader, uniformModel;
-
-bool direction = true;
-float triOffset = 0.0f;
-float triMaxOffset = 0.7f;
-float triIncrement = 0.005f;
-
-float curAngle = 0.0f;
-
-bool sizeDirection = true;
-float curSize = 0.4f;
-float maxSize = 0.8f;
-float minSize = 0.1f;
 
 // Vertex Shader
 static const char* vShader =
@@ -33,10 +20,13 @@ static const char* vShader =
     "\n"
     "layout (location = 0) in vec3 pos;\n"
     "\n"
+    "out vec4 vCol;\n"
+    "\n"
     "uniform mat4 model;\n"
     "\n"
     "void main() {\n"
     "   gl_Position = model * vec4(pos.x, pos.y, pos.z, 1.0);\n"
+        "vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);\n"
     "}\0";
 
 static const char* fShader =
@@ -44,8 +34,10 @@ static const char* fShader =
     "\n"
     "out vec4 colour;\n"
     "\n"
+    "in vec4 vCol;\n"
+    "\n"
     "void main() {\n"
-    "   colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "   colour = vCol;\n"
     "}\0";
 
 void createTriangle() {
@@ -174,36 +166,13 @@ int main() {
     while(!glfwWindowShouldClose(mainWindow)) {
         glfwPollEvents();
 
-        if (direction)
-            triOffset += triIncrement;
-        else
-            triOffset -= triIncrement;
-
-        if (std::abs(triOffset) >= triMaxOffset)
-            direction = !direction;
-
-        curAngle += 0.05f;
-        if (curAngle >= 360) {
-            curAngle -= 360;
-        }
-
-        if (sizeDirection)
-            curSize += 0.001f;
-        else
-            curSize -= 0.001f;
-
-        if (curSize >= maxSize || curSize <= minSize)
-            sizeDirection = !sizeDirection;
-
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
 
         glm::mat4 model(1.0f);
-        model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
